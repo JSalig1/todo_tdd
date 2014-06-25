1bd230f69ea332d60dc048f3474ff21864340cf0
jsalig1 <jeremy.salig@gmail.com>
2014-06-25 14:34:22 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoTdd::Application.config.secret_key_base = '68fc57d6fb8bc8e5898f48c55d7e284695246d677f2920b5b694a7d94da87bd69727af69a4ad50953c6c959f31c7f4b36ac99f6523f25d2bb771e7d1d6aac78b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TodoTdd::Application.config.session_store :cookie_store, key: '_todo_tdd_session'
